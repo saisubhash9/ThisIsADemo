ae0f1990d183606cd609cdf916715b85f04d03f4
saisubhash9 <saisubhash654@gmail.com>
2020-09-16 21:01:47 +0530

    first code file

---
+++ test.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+void main()
+{
+	char c[50],b[50];
+	printf("initialising system\n");
+	printf("waiting for system\n");
+	printf("enter mail\n");
+	scanf("%s\n\n",&c);
+	printf("----------\n");
+	printf("enter new password\n");
+	scanf("%s",&b);
+	printf("taking control of %s",c);
+	printf("waiting for google server....\n");
+	printf("hacked %s",c);
+    printf("------------------");
+    printf("password updated");
+}
